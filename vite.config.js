2867d1177b5dec824806d23777cefea201321b4f
DenmarkGod <145672238+DenmarkGod@users.noreply.github.com>
2026-07-24 15:42:45 +0300

    vite.config.js

--- vite.config.js
+++
@@ -1,1 +0,0 @@
-import { defineConfig } from 'vite' import react from '@vitejs/plugin-react' import { ghPages } from 'vite-plugin-gh-pages'  export default defineConfig({   plugins: [     react(),     ghPages()   ],   base: '/analyzer/',   build: {     outDir: 'dist',     rollupOptions: {       input: {         main: './src/index.html',       },     },   }, }) 
